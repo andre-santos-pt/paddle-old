33a03c9e9a2b67b93673aa2d372e5a91f395ecda
Francisco Alfredo <falfredo@visor.ai>
2020-04-06 22:41:24 +0100

    Removed unnecessary check.

--- pt.iscte.paddle.model/src/pt/iscte/paddle/model/IProcedure.java
+++ pt.iscte.paddle.model/src/pt/iscte/paddle/model/IProcedure.java
@@ -15,23 +15,23 @@ public interface IProcedure extends IProcedureDeclaration {
 	List<IVariableDeclaration> getLocalVariables();
 	List<IVariableDeclaration> getVariables();
 	IType getReturnType();
-	
+
 	IBlock getBody();
-	
+
 	default IControlFlowGraph generateCFG(){
 		IControlFlowGraph cfg = IControlFlowGraph.create(this);
 		IVisitor visitor = new Visitor(cfg);
-		
+
 		this.accept(visitor);
-		
-		if(this.getReturnType() == IType.VOID)
-			cfg.getNodes().forEach(node -> {
+
+		cfg.getNodes().forEach(node -> {
+			System.out.println("node: " + node.getNext());
 			if(node.getNext() == null && !node.isExit()) node.setNext(cfg.getExitNode());
 		});
-		
+
 		return cfg;
 	}
-	
+
 	default boolean isRecursive() {
 		class RecFind implements IVisitor {
 			boolean foundRecursiveCall = false;
@@ -46,39 +46,39 @@ public interface IProcedure extends IProcedureDeclaration {
 		getBody().accept(r);
 		return r.foundRecursiveCall;
 	}
-	
+
 	default IVariableDeclaration getVariable(String id) {
 		for(IVariableDeclaration v : getVariables())
 			if(id.equals(v.getId()))
 				return v;
 		return null;
 	}
-	
-//	default boolean isFunction() {
-//		if(getReturnType().isVoid())
-//			return false;
-//	}
-//	
-//	default boolean isStateLessFunction() {
-//		if(getReturnType().isVoid())
-//			return false;
-//	}
-	
+
+	//	default boolean isFunction() {
+	//		if(getReturnType().isVoid())
+	//			return false;
+	//	}
+	//	
+	//	default boolean isStateLessFunction() {
+	//		if(getReturnType().isVoid())
+	//			return false;
+	//	}
+
 	default void accept(IBlock.IVisitor visitor) {
 		getBody().accept(visitor);
 	}
-	
+
 	default String translate(IModel2CodeTranslator t) {
 		return t.header(this) + t.statements(getBody()) + t.close(this);
 	}
-	
+
 	public class UnboundProcedure implements IProcedure {
 		private String id;
-		
+
 		public UnboundProcedure(String id) {
 			this.id = id;
 		}
-		
+
 		@Override
 		public String getId() {
 			return id;
@@ -132,6 +132,6 @@ public interface IProcedure extends IProcedureDeclaration {
 		public IBlock getBody() {
 			return null;
 		}
-		
+
 	}
 }
