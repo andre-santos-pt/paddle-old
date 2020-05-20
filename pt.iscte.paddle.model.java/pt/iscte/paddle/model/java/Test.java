7f54948f754aa49360e42eb607a0543625463f80
André L. Santos <andre.santos@iscte.pt>
2020-05-20 10:41:36 +0100

    parser improvements

--- pt.iscte.paddle.model.java/pt/iscte/paddle/model/java/Test.java
+++ pt.iscte.paddle.model.java/pt/iscte/paddle/model/java/Test.java
@@ -2,22 +2,56 @@ package pt.iscte.paddle.model.java;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 import pt.iscte.paddle.model.IModule;
 
 public class Test {
 
-	public void test() {
-		
-		
-	}
 	public static void main(String[] args) throws IOException {
-//		String path = "/Users/andresantos/git/paddle-ui/pt.iscte.paddle.javardise.tests/src-gen";
-		String path = "/Users/andresantos/Desktop/Trabalhos/2";
-		JavaParser p = new JavaParser(new File(path), f -> f.getName().equals("ImageUtil.java"));
-		IModule m = p.parse();
-		System.out.println(m);
-		
-	}
+		//		Charset latin = Charset.forName("ISO-8859-1");
+		//		Charset utf8 = Charset.forName("UTF-8");
+		//		Charset win = Charset.forName("windows-1252");
 
+		//		String path = "/Users/andresantos/git/paddle-ui/pt.iscte.paddle.javardise.tests/src-gen";
+		File root = new File("/Users/andresantos/Desktop/Trabalhos254/4");
+//		File root = new File("/Users/andresantos/EclipseWS/workspace-paddle/student/src");
+		List<File> compilationOk = new ArrayList<>();
+		List<IModule> parseOk = new ArrayList<>();
+		List<String> errorProjs = new ArrayList<>();
+		
+		for (int n = 2; n <= 2; n++) {
+			File proj = new File(root, ""+n);
+
+			JavaParser p = new JavaParser(root, f -> f.getName().equals("ImageUtil.java"));
+			if(p.check()) {
+				compilationOk.add(proj);
+//				 try {
+					IModule m = p.parse();
+					parseOk.add(m);
+					System.out.println(m);
+//				}
+//				catch(Exception e) {
+//					errorProjs.add(""+n);
+//					e.printStackTrace();
+//				}
+			}
+			//					success.add(proj);
+			//			System.out.println(proj + " : " + (m == null ? "fail" : "ok"));
+		}
+		System.out.println("comp " + compilationOk.size());
+		System.out.println("parse " + parseOk.size());
+		System.out.println(errorProjs);
+		//		if(m == null)
+		//			System.err.println("file errors on " + path);
+		//		System.out.println(m);
+	}
 }
+
+//		SemanticChecker checker = new SemanticChecker(new AsgSemanticChecks());
+//		List<ISemanticProblem> problems = checker.check(module);
+//	    System.err.println(problems);
+
+
